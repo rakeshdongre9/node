b35d63d13baabd6b1507e4a4335c6aaf805ab065
RAKESH DONGRE <112848752+rakeshdongre9@users.noreply.github.com>
2023-12-20 10:23:19 +0530

    Update upload-image-optional.js

--- upload-image-optional.js
+++ upload-image-optional.js
@@ -28,6 +28,25 @@ const upload = multer({
 
 
 // Handle the POST request with multiple images
+app.post('/upload', upload.fields([{ name: 'images', maxCount: 5 }, { name: 'files', maxCount: 3 }]), (req, res) => {
+  // Access the uploaded files in the request object
+  const imagesArray = req.files;
+
+  // Process or save the images as needed
+  // For demonstration, log the number of uploaded images
+  console.log('Number of uploaded images:', imagesArray.length);
+
+  // Send a response
+  res.status(200).json({ message: 'Images uploaded successfully.' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+// Handle the POST request with multiple images
 app.post('/upload',  upload.array('images', 5), (req, res) => {
   // Access the uploaded files in the request object
   const imagesArray = req.files;
